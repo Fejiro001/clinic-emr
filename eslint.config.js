154ff8f66bd496870d23b44ec6f470af83c0d7c8
Fejiro001 <aberefejiro@gmail.com>
2025-10-20 18:20:02 +0100

    Update environment variables, enhance ESLint configuration, modify HTML title, and add electron dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,49 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores([
+    "dist",
+    "dist-electron",
+    "node_modules",
+    "electron-builder.json",
+  ]),
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}"],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
+      tseslint.configs.recommendedTypeChecked,
+      tseslint.configs.strictTypeChecked,
+      tseslint.configs.stylisticTypeChecked,
+      reactHooks.configs["recommended-latest"],
       reactRefresh.configs.vite,
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
   },
-])
+  // {
+  //   files: ["electron/**/*.ts"],
+  //   extends: [js.configs.recommended, tseslint.configs.recommended],
+  //   languageOptions: {
+  //     ecmaVersion: 2020,
+  //     globals: globals.node,
+  //     parserOptions: {
+  //       projectService: {
+  //         allowDefaultProject: ["electron/**/*.ts"],
+  //       },
+  //       tsconfigRootDir: import.meta.dirname,
+  //     },
+  //   },
+  // },
+]);
